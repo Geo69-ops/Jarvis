4be3f48d0ebcc862e6c18083675062eb2c6843c4
Geo69-ops <82852931+Geo69-ops@users.noreply.github.com>
2024-03-13 18:14:01 -0500

    Integración ElevenLabs

--- Jarvis.py
+++ Jarvis.py
@@ -1,47 +1,104 @@
+import playsound
+from gtts import gTTS 
+
+
+from elevenlabs import generate, play, set_api_key
+
+
+
 import speech_recognition as sr
 import pyttsx3
 
 import os
 import pyaudio
+import openai
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
-keyy = "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"
+API_KEY = os.environ.get("API_KEYS_ELEVENLABS")
+set_api_key = API_KEY
 
+API_KEY_OPENAI = os.environ.get("APIKEYS_CHAT_GPT")
+
+lang = 'en'
 # OPENAI_KEY = os.getenv('OPENAI_KEY')
 
-OPENAI_KEY = keyy
 
-import openai
-openai.api_key = OPENAI_KEY
+# OPENAI_KEY = keyy
+# openai.api_key = OPENAI_KEY
 
-def SpeakText(command):
-    engine= pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
+def get_audio():
+        r= sr.Recognizer()
+        with sr.Microphone(device_index=1) as source:
+            audio = r.listen(source)
+            said = ""
 
-r= sr.Recognizer()
+            try:
+                said= r.recognize_google(audio)
+                print(said)
 
-def record_text():
-    while(1):
-        try:
-            with sr.Microphone() as source:
-                r.adjust_for_ambient_noise(source,duration=0.2)
-                print ("i'm listening")
+                if "Friday" in said:
+                    # completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-0125", messages={"role": "user", "content": said})
+                    # text = completion.choices[0].message.content
+                    texto = "Hola estúpido humano, en qué puedo servirte"
+                    audio2 = generate(
+                         text=texto,
+                         voice="Sam",
+                         model= 'eleven_multilingual_v1'
+                    )
+
+                    # audio2.save("output.wav")
+                    
+                    play(audio2)
+
+                    speech = gTTS(text=texto, lang=lang, slow=False,tld="com.co")
+                    speech.save("welcome.mp3")
+                    playsound.playsound("welcome.mp3")
+                    os.remove("welcome.mp3")
+            except Exception as e:
+                print(e)
+        return said 
+
+while True:
+    get_audio()
+
+
+
+
+
+
+# def SpeakText(command):
+#     engine= pyttsx3.init()
+#     engine.say(command)
+#     engine.runAndWait()
+
+# r= sr.Recognizer()
+
+
+
+
+# def record_text():
+#     while(1):
+#         try:
+#             with sr.Microphone() as source:
+#                 r.adjust_for_ambient_noise(source,duration=0.2)
+#                 print ("i'm listening")
                 
-                audio2= r.listen(source)
+#                 audio2= r.listen(source)
 
-                MyText = r.recognize_google(audio2)
-                print(MyText)
-                return MyText
+#                 MyText = r.recognize_google(audio2)
+#                 print(MyText)
+#                 return MyText
                 
             
-        except sr.RequestError as e:
-            print("could not request results; {0}", format(e))
+#         except sr.RequestError as e:
+#             print("could not request results; {0}", format(e))
         
-        except sr.UnknownValueError:
-            print("Unknow error ocurred")
-record_text()
+#         except sr.UnknownValueError:
+#             print("Unknow error ocurred")
+# record_text()
 
 # def send_to_chatGPT(messages, model="gpt-3.5-turbo"):
 #     response= openai.ChatCompletion.create(
